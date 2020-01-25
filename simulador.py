ea235334013068f0d27e2e322f63e276e0fad06c
Maria Fernanda <55985690+scarletalex@users.noreply.github.com>
2020-01-25 08:19:19 -0300

    Delete simulador.py

--- simulador.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-while(True) :
-   valor = int(input("Informe um valor: "))
-   r = requests.post("https://sunflower-engine.herokuapp.com/upload", json={"VOLT":valor})
-   print(r.status_code)
